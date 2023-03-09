6ac9f8db7efe9d8b4e88522bdc588f68123e6e7d
Lucas <contatolucasm9@gmail.com>
2023-03-08 23:44:34 -0300

    newsletter

--- src/app/components/bar-bottom/bar-bottom.component.ts
+++ src/app/components/bar-bottom/bar-bottom.component.ts
@@ -7,18 +7,39 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class BarBottomComponent implements OnInit {
 
-  @Input()
-  icon_L:string=''
-  @Input()
-  icon_G:string=''
-  @Input()
-  icon_F:string=''
+  
 
   constructor() { }
 
   ngOnInit(): void {
+    
+    const form = document.querySelector('.cadastro') as HTMLFormElement;
+    const button = document.querySelector('.btn') as HTMLButtonElement;
+
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      const emailInput = document.querySelector('.insc') as HTMLInputElement;
+      const checkbox = document.querySelector('.check') as HTMLInputElement;
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      if (checkbox.checked && emailRegex.test(emailInput.value)) {
+        window.alert('Parabéns! Agora você faz parte da nossa newsletter.');
+        emailInput.value = "";
+        checkbox.checked = false;
+        }
+
+        else {
+          window.alert('Por favor, marque a check-box ou insira um email válido para continuar.')
+        }
+
+      
+    
+
+        
+
+      
   
-    }
+
+  })
   }
-
-
+}
